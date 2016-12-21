d5571ccc2f4ed9c257c1959cb26a8dbe1ec9d558
Florian Courgey <florian@floriancourgey.com>
2016-12-21 15:44:10 +0100

    + separation des 3 images

--- programmation/Visual Vigenere/main.py
+++ programmation/Visual Vigenere/main.py
@@ -3,4 +3,38 @@
 
 from fonctions import get, post
 
-print('abc')
+import binascii
+import re
+from PIL import Image
+import io
+
+chemin = '/home/florian/dev/challenges/newbiecontest/programmation/Visual Vigenere/imgcrypted.png'
+pngMagicNumbers = '89504e470d0a1a0a'
+
+f = open(chemin, 'rb')
+content = f.read()
+texte = binascii.hexlify(content).decode('ascii')
+# print(texte)
+nombreDePng = texte.count(pngMagicNumbers)
+if nombreDePng != 3:
+    exit('Il y a '+nombreDePng+' PNG au lieu de 3')
+# on recupère chaque images
+images = texte.split(pngMagicNumbers)
+pngStrings = []
+pngs = []
+# on ajoute le suffixe
+for image in images:
+    if image:
+        pngStrings += [pngMagicNumbers+image]
+# on convertit chaque pngString en Image Pillow
+for i, pngString in enumerate(pngStrings):
+    fichier = 'png'+str(i+1)+'.png'
+    fout = open(fichier, 'wb')
+    fout.write(binascii.unhexlify(pngString))
+    fout.close()
+    try:
+        im = Image.open(fichier)
+        pngs += [im]
+    except Exception as e:
+        print("erreur : ", e)
+        exit()
